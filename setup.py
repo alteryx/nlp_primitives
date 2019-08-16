7d341d22f8ae19d50c83cc7d86ab726e295b01a3
Clara Duffy <ctduffy@college.harvard.edu>
2019-08-16 15:48:21 -0400

    removed encoding for description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 
