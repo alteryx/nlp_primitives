a3c18a3b53bc020dff9ad0a56224a2ffa89a93b2
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2019-11-21 11:04:12 -0600

    Universal Sentence Encoder Test
    The test was updated to check whether it would timeout on Circle CI. Releated to 964dc96782121ca226af720ab18ed2a70f9f9e37.

--- nlp_primitives/tests/test_universal_sentence_encoder.py
+++ nlp_primitives/tests/test_universal_sentence_encoder.py
@@ -1,6 +1,4 @@
-import numpy as np
 import pandas as pd
-import pytest
 
 from ..universal_sentence_encoder import UniversalSentenceEncoder
 from ..utils import PrimitiveT
@@ -9,7 +7,6 @@ from ..utils import PrimitiveT
 class TestUniversalSentenceEncoder(PrimitiveT):
     primitive = UniversalSentenceEncoder
 
-    @pytest.mark.skip()
     def test_regular(self):
         primitive = self.primitive().get_function()
         sentences = pd.Series([
@@ -19,24 +16,4 @@ class TestUniversalSentenceEncoder(PrimitiveT):
             "When will humans go to mars?",
             "Mitochondria is the powerhouse of the cell",
         ])
-        new_results = primitive(sentences)
-        assert round(sum(new_results[:, 0]), 3) == -0.383
-        assert round(sum(new_results[:, 1]), 3) == 1.643
-        assert round(sum(new_results[:, 2]), 3) == 0.950
-        assert round(sum(new_results[:, 3]), 3) == 0.423
-        assert round(sum(new_results[:, 4]), 3) == 1.451
-        answer0 = new_results[:5, 0].round(5)
-        answer1 = new_results[:5, 1].round(5)
-        answer2 = new_results[:5, 2].round(5)
-        answer3 = new_results[:5, 3].round(5)
-        answer4 = new_results[:5, 4].round(5)
-        correct0 = np.array([0.005107, -0.011806, -0.048946, 0.005020, 0.019708]).round(5)
-        correct1 = np.array([0.017786, 0.061581, -0.008888, 0.034359, 0.044404]).round(5)
-        correct2 = np.array([-0.007204, 0.039610, -0.055290, 0.037506, 0.009574]).round(5)
-        correct3 = np.array([-0.059914, 0.008037, -0.053351, 0.018910, 0.031320]).round(5)
-        correct4 = np.array([-0.029039, 0.071482, 0.001880, 0.052682, -0.047268]).round(5)
-        np.testing.assert_allclose(correct0, answer0, rtol=1e-04)
-        np.testing.assert_allclose(correct1, answer1, rtol=1e-04)
-        np.testing.assert_allclose(correct2, answer2, rtol=1e-04)
-        np.testing.assert_allclose(correct3, answer3, rtol=1e-04)
-        np.testing.assert_allclose(correct4, answer4, rtol=1e-04)
+        primitive(sentences)
