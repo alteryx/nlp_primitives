7f73eaad8c35456b960302064236b7b19c11caf7
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2019-11-21 12:17:19 -0600

    Renamed Function

--- nlp_primitives/conftest.py
+++ nlp_primitives/conftest.py
@@ -9,5 +9,5 @@ def universal_sentence_encoder():
 
 
 @pytest.fixture(autouse=True)
-def add_labels(doctest_namespace, universal_sentence_encoder):
+def add_primitives(doctest_namespace, universal_sentence_encoder):
     doctest_namespace['universal_sentence_encoder'] = universal_sentence_encoder
