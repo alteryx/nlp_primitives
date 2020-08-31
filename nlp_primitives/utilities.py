651ec1503fe7d8de402189279fbf717bbc5903f3
Becca McBrayer <rebecca.mcbrayer@gmail.com>
2020-08-31 16:16:00 -0400

    Rework clean_text to run when download(s) fail

--- nlp_primitives/utilities.py
+++ nlp_primitives/utilities.py
@@ -8,24 +8,25 @@ wn = nltk.WordNetLemmatizer()
 
 def clean_tokens(textstr):
     try:
+        textstr = nltk.tokenize.word_tokenize(textstr)
+    except LookupError:
+        nltk.download('punkt')
+        textstr = nltk.tokenize.word_tokenize(textstr)
+
+    try:
         swords = set(nltk.corpus.stopwords.words('english'))
+        textstr = [ch.lower() for ch in textstr if ch not in tset(string.punctuation).union(swords)]
     except LookupError:
         nltk.download('stopwords')
         swords = set(nltk.corpus.stopwords.words('english'))
+        textstr = [ch.lower() for ch in textstr if ch not in set(string.punctuation).union(swords)]
+
+    try:
+        textstr = [wn.lemmatize(w) for w in textstr]
+    except LookupError:
+        nltk.download('wordnet')
+        textstr = [wn.lemmatize(w) for w in textstr]
+
     finally:
-        try:
-            textstr = nltk.tokenize.word_tokenize(textstr)
-        except LookupError:
-            nltk.download('punkt')
-            textstr = nltk.tokenize.word_tokenize(textstr)
-        finally:
-            try:
-                wn.lemmatize("")
-            except LookupError:
-                nltk.download('wordnet')
-            finally:
-                processed = [ch.lower() for ch in textstr if ch not in
-                             set(string.punctuation).union(swords)]
-                processed = ['0' if re.search('[0-9]+', ch) else ch for ch in processed]
-                processed = [wn.lemmatize(w) for w in processed]
-                return processed
+        textstr = ['0' if re.search('[0-9]+', ch) else ch for ch in textstr]
+        return textstr
