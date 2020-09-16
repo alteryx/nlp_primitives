cbd5172a28d60c92633e4f2c43e4f04103fa63c4
Becca McBrayer <rebecca.mcbrayer@gmail.com>
2020-09-16 10:40:13 -0400

    Revert to using os - pathlib causes errors in python 3.6

--- nlp_primitives/utilities.py
+++ nlp_primitives/utilities.py
@@ -1,7 +1,7 @@
+import os
 import shutil
 import string
 import tempfile
-from pathlib import Path
 
 import nltk
 
@@ -23,17 +23,16 @@ def clean_tokens(textstr):
 
 
 def unpack_data():
-    if not Path(nltk.data.path[0]).exists():
-        fp = (Path(__file__) / '../../data/nltk-data.tar.gz').resolve()
-        dp = (fp / '../nltk-data').resolve()
-        nltk.data.path.append(dp / 'nltk-data')
+    if not os.path.exists(nltk.data.path[0]):
+        fp = os.path.normpath(os.path.join(os.path.realpath(__file__), '../../data/nltk-data.tar.gz'))
+        dp = os.path.normpath(os.path.join(fp, '../nltk-data'))
+        nltk.data.path.append(os.path.normpath(os.path.join(fp, '../nltk-data/nltk-data')))
         try:
             tf = tempfile.mkdtemp()
             shutil.unpack_archive(fp, tf)
-            if dp.exists():
+            if os.path.exists(dp):
                 shutil.rmtree(dp)
             shutil.copytree(tf, dp)
-            print('Unloaded nltk data to', dp)
         finally:
             shutil.rmtree(tf)
 
