6a7550481c03c34d8fdcb0c22f3ac27844d7adee
Becca McBrayer <60074782+eccabay@users.noreply.github.com>
2020-09-17 14:20:46 -0400

    Refactor `clean_tokens` function (#29)
    * Fix failing universal sentence encoder tests
    
    * Rework clean_text to run when download(s) fail
    
    * Remove word tokenizer
    
    * Remove detokenizer from polarity score
    
    * Replace re.search with map in clean_tokens

--- nlp_primitives/utilities.py
+++ nlp_primitives/utilities.py
@@ -1,4 +1,3 @@
-import re
 import string
 
 import nltk
@@ -7,25 +6,25 @@ wn = nltk.WordNetLemmatizer()
 
 
 def clean_tokens(textstr):
+    textstr = textstr.translate(str.maketrans('', '', string.punctuation))
+    textstr = [ch for ch in textstr.split(' ') if len(ch) > 0]
+
     try:
         swords = set(nltk.corpus.stopwords.words('english'))
+        to_remove = set(string.punctuation).union(swords)
+        textstr = [ch.lower() for ch in textstr if ch not in to_remove]
     except LookupError:
         nltk.download('stopwords')
         swords = set(nltk.corpus.stopwords.words('english'))
+        to_remove = set(string.punctuation).union(swords)
+        textstr = [ch.lower() for ch in textstr if ch not in to_remove]
+
+    try:
+        textstr = [wn.lemmatize(w) for w in textstr]
+    except LookupError:
+        nltk.download('wordnet')
+        textstr = [wn.lemmatize(w) for w in textstr]
+
     finally:
-        try:
-            textstr = nltk.tokenize.word_tokenize(textstr)
-        except LookupError:
-            nltk.download('punkt')
-            textstr = nltk.tokenize.word_tokenize(textstr)
-        finally:
-            try:
-                wn.lemmatize("")
-            except LookupError:
-                nltk.download('wordnet')
-            finally:
-                processed = [ch.lower() for ch in textstr if ch not in
-                             set(string.punctuation).union(swords)]
-                processed = ['0' if re.search('[0-9]+', ch) else ch for ch in processed]
-                processed = [wn.lemmatize(w) for w in processed]
-                return processed
+        textstr = ['0' if any(map(str.isdigit, ch)) else ch for ch in textstr]
+        return textstr
