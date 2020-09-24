7d1479b14266fc0cc2eaa69e90243bb5736e58c4
Dylan Sherry <sharshofski@gmail.com>
2020-09-24 02:46:02 -0400

    Unpack data tarball before creating sdist

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from os import path
 
 from setuptools import find_packages, setup
+from setuptools.command.sdist import sdist
+
+import pathlib
+import pkg_resources
+import tarfile
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md')) as f:
@@ -9,6 +14,19 @@ with open(path.join(this_directory, 'README.md')) as f:
 extras_require = {
     'complete': open('complete-requirements.txt').readlines()
 }
+
+class PreSDistCommand(sdist):
+    """Post-installation for installation mode."""
+    def run(self):
+        nltk_data_tarball_path = pkg_resources.resource_filename('nlp_primitives', str(pathlib.Path('data','nltk-data.tar.gz')))
+        nltk_data_extract_path = pkg_resources.resource_filename('nlp_primitives', str(pathlib.Path('data')))
+        print(f'Extracting nltk data files from {nltk_data_tarball_path} to {nltk_data_extract_path}')
+        print(pkg_resources.resource_listdir('nlp_primitives', str(pathlib.Path('data'))))
+        tar = tarfile.open(nltk_data_tarball_path, "r:gz")
+        tar.extractall(path=nltk_data_extract_path)
+        tar.close()
+        print(f'Extraction of nltk data files complete')
+        sdist.run(self)
 
 setup(
     name='nlp_primitives',
@@ -19,6 +37,8 @@ setup(
     url='http://www.featurelabs.com/',
     install_requires=open('requirements.txt').readlines(),
     packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
     long_description=long_description,
     long_description_content_type='text/markdown',
     python_requires='>=3.6',
@@ -28,4 +48,7 @@ setup(
             'nlp_primitives = nlp_primitives',
         ],
     },
+    cmdclass={
+        'sdist': PreSDistCommand
+    }
 )
