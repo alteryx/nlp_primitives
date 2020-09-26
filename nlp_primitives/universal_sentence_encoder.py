50c16f5a9f05e6c7f4913ade89064c689a574871
Dylan Sherry <sharshofski@gmail.com>
2020-09-25 22:39:59 -0400

    Fix docstring bug

--- nlp_primitives/universal_sentence_encoder.py
+++ nlp_primitives/universal_sentence_encoder.py
@@ -12,7 +12,7 @@ class UniversalSentenceEncoder(TransformPrimitive):
 
     Examples:
         >>> sentences = ["I like to eat pizza", "The roller coaster was built in 1885.", ""]
-        >>> output = universal_sentence_encoder(sentences)
+        >>> output = UniversalSentenceEncoder(sentences)
         >>> len(output)
         512
         >>> len(output[0])
