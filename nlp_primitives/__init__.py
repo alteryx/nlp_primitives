985997da2af7c6a556f83173f8074cfe11e83d75
Nate Parsons <nate.parsons@alteryx.com>
2021-10-12 16:43:31 -0500

    v2.0.0.dev0

--- nlp_primitives/__init__.py
+++ nlp_primitives/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 import nltk.data
 
-__version__ = '2.0.0rc1'
+__version__ = '2.0.0.dev0'
 import pkg_resources
 
 from .diversity_score import DiversityScore
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extras_require = {
 
 setup(
     name='nlp_primitives',
-    version='2.0.0rc1',
+    version='2.0.0.dev0',
     author='Feature Labs, Inc.',
     author_email='support@featurelabs.com',
     classifiers=[
