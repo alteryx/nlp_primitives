f8058856bc93c618e7cba4efb48986fc3fd1937f
Gaurav Sheni <gvsheni@gmail.com>
2021-12-21 13:37:00 -0500

    update version to dev

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extras_require = {
 
 setup(
     name='nlp_primitives',
-    version='2.0.0',
+    version='2.1.0.dev',
     author='Alteryx, Inc.',
     author_email='support@featurelabs.com',
     classifiers=[
