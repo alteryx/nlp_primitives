dae934e62aef8f921be95f83edc8f5704b8a95de
sbadithe <slyokapi@gmail.com>
2022-08-04 09:26:12 -0700

    refactor re.escape

--- nlp_primitives/number_of_unique_words.py
+++ nlp_primitives/number_of_unique_words.py
@@ -55,7 +55,8 @@ class NumberOfUniqueWords(TransformPrimitive):
         def num_unique_words(array):
             if self.case_insensitive:
                 array = array.str.lower()
-            delims = re.escape("- \[\],!\?;\n\t")
+            delims = re.escape("- [],!?;")
+            delims = delims + r"\n\t"
             array = array.str.split(f"[{delims}]")
             return array.apply(_unique_word_helper)
 
