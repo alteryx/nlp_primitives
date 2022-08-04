a8aaeb9123750a463797a4f68fdec48b180c65fd
sbadithe <slyokapi@gmail.com>
2022-08-04 13:14:33 -0700

    Remove unused import

--- nlp_primitives/number_of_sentences.py
+++ nlp_primitives/number_of_sentences.py
@@ -2,7 +2,6 @@
 from typing import Iterable
 
 import numpy as np
-import pandas as pd
 from featuretools.primitives.base import TransformPrimitive
 from nltk.tokenize import sent_tokenize
 from woodwork.column_schema import ColumnSchema
