6470f3321cdd2c62e349835312bbea1232c85d11
sbadithe <shripad.badithe@alteryx.com>
2022-10-12 12:00:03 -0700

    init

--- nlp_primitives/utilities.py
+++ nlp_primitives/utilities.py
@@ -1,19 +1,19 @@
 import string
-
 import nltk
 
 
 def clean_tokens(textstr):
-    wn = nltk.WordNetLemmatizer()
-
     textstr = textstr.translate(str.maketrans("", "", string.punctuation))
-    textstr = [ch for ch in textstr.split(" ") if len(ch) > 0]
+    textstr = [ch.lower() for ch in textstr.split(" ") if len(ch) > 0]
 
-    swords = set(nltk.corpus.stopwords.words("english"))
-    to_remove = set(string.punctuation).union(swords)
-    textstr = [ch.lower() for ch in textstr if ch not in to_remove]
+    # Remove stopwords and punctuation
+    stopwords_and_punctuation = set(nltk.corpus.stopwords.words("english")).union(set(string.punctuation))
+    textstr = [ch for ch in textstr if ch not in stopwords]
 
+    # Lemmatize tokens
+    wn = nltk.WordNetLemmatizer()
     textstr = [wn.lemmatize(w) for w in textstr]
 
+    # Set all tokens with a digit in them to '0'
     textstr = ["0" if any(map(str.isdigit, ch)) else ch for ch in textstr]
     return textstr
