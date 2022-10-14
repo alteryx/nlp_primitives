2da094433be329a7b77eb9c43052287beff46bbb
sbadithe <shripad.badithe@alteryx.com>
2022-10-13 17:30:12 -0700

    use 'word' instead of 'ch'

--- nlp_primitives/utilities.py
+++ nlp_primitives/utilities.py
@@ -5,18 +5,18 @@ import nltk
 
 def clean_tokens(textstr):
     textstr = textstr.translate(str.maketrans("", "", string.punctuation))
-    textstr = [ch.lower() for ch in textstr.split(" ") if len(ch) > 0]
+    textstr = [word.lower() for word in textstr.split(" ") if len(word) > 0]
 
     # Remove stopwords and punctuation
     stopwords_and_punctuation = set(nltk.corpus.stopwords.words("english")).union(
         set(string.punctuation)
     )
-    textstr = [ch for ch in textstr if ch not in stopwords_and_punctuation]
+    textstr = [word for word in textstr if word not in stopwords_and_punctuation]
 
     # Lemmatize tokens
     wn = nltk.WordNetLemmatizer()
-    textstr = [wn.lemmatize(w) for w in textstr]
+    textstr = [wn.lemmatize(word) for word in textstr]
 
     # Set all tokens with a digit in them to '0'
-    textstr = ["0" if any(map(str.isdigit, ch)) else ch for ch in textstr]
+    textstr = ["0" if any(map(str.isdigit, word)) else word for word in textstr]
     return textstr
