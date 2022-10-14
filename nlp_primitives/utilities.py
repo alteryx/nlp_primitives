2f8c3b965cc9b7d0e62f07e106a54a882cae33a7
sbadithe <shripad.badithe@alteryx.com>
2022-10-13 19:48:37 -0700

    add typing hint

--- nlp_primitives/utilities.py
+++ nlp_primitives/utilities.py
@@ -1,22 +1,23 @@
 import string
+from typing import List
 
 import nltk
 
 
-def clean_tokens(textstr):
-    textstr = textstr.translate(str.maketrans("", "", string.punctuation))
-    textstr = [word.lower() for word in textstr.split(" ") if len(word) > 0]
+def clean_tokens(text: str) -> List[str]:
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    text = [word.lower() for word in text.split(" ") if len(word) > 0]
 
     # Remove stopwords and punctuation
     stopwords_and_punctuation = set(nltk.corpus.stopwords.words("english")).union(
         set(string.punctuation)
     )
-    textstr = [word for word in textstr if word not in stopwords_and_punctuation]
+    text = [word for word in text if word not in stopwords_and_punctuation]
 
     # Lemmatize tokens
     wn = nltk.WordNetLemmatizer()
-    textstr = [wn.lemmatize(word) for word in textstr]
+    text = [wn.lemmatize(word) for word in text]
 
     # Set all tokens with a digit in them to '0'
-    textstr = ["0" if any(map(str.isdigit, word)) else word for word in textstr]
-    return textstr
+    text = ["0" if any(map(str.isdigit, word)) else word for word in text]
+    return text
